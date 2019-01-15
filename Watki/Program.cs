13f5a5b8b70a2b704e55d05f777a9c34d9772b6d
Marian Marian <fenrir55@gmail.com>
2019-01-15 19:52:00 +0100

    v1

--- Watki/Program.cs
+++ Watki/Program.cs
@@ -3,13 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Watki
 {
     class Program
     {
+        static readonly Object locker = new Object();
+        static bool wartosc = false;
         static void Main(string[] args)
         {
+            Thread thread1 = new Thread(() => wypisz(3));
+            Thread thread2 = new Thread(() => wypisz3(5));
+            Thread thread3 = new Thread(() => wypisz(4));
+            thread1.Start();
+           // thread1.Join();
+            thread2.Start();
+            thread3.Start();
+            wypisz2(2);
+
+            Task<string> task = Task.Run(() =>
+            {
+                return "task";
+            });
+        
+            string wyrazenie = task.Result;
+            Console.WriteLine(wyrazenie);
+
+            Console.ReadKey();
+        }
+
+        static void wypisz(int ile)
+        {
+            lock (locker) {
+                if (wartosc)
+                {
+                    for (int i = 0; i < ile; i++)
+                    {
+                        Console.WriteLine("Done");
+                    }
+                }
+                wartosc = true;
+            }
+        }
+        static void wypisz2(int ile)
+        {
+            for (int i = 0; i < ile; i++)
+            {
+                Console.WriteLine("A");
+            }
+        }
+        static void wypisz3(int ile)
+        {
+            for (int i = 0; i < ile; i++)
+            {
+                Console.WriteLine("D");
+            }
         }
     }
+  
 }
